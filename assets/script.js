fcefb05fe8ed47ec57646a12d7821beed04a0e21
Erv <marcnicolas0915@gmail.com>
2022-12-15 21:39:12 -0500

    added files

---
+++ assets/script.js
@@ -0,0 +1,184 @@
+key = 'eadb20433e6df59a3eca6031b67b3de7'
+
+var current = dayjs();
+var currentDate = current.format('YYYY/MM/DD');
+var currentDateApi = current.format('YYYY/MM/DD');
+
+
+var getFiveDayForecast = async (lat, lon) => {
+    var base = 'https://api.openweathermap.org/data/2.5/forecast';
+    var query = `?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
+
+    var response = await fetch(base + query);
+    var data = await response.json();
+
+    
+    return data;
+};
+
+// get current weather with city, pass lat and lon onto getFiveDayForecast
+var getCurrentWeather = async (city) => {
+    var base = 'https://api.openweathermap.org/data/2.5/weather';
+    var query = `?q=${city}&units=imperial&appid=${key}`;
+
+    var response = await fetch(base + query);
+
+    if (!response.ok) {
+        localStorage.removeItem(city);
+    }
+    
+    var data = await response.json();
+
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+    
+    updateFiveDayForecast(lat, lon);
+    
+    return data;
+};
+
+
+
+var updateCurrentWeather = async (city) => {
+    
+    getCurrentWeather(city).then(data => {
+
+        clearAll();
+
+        var iconCode = data.weather[0].icon;
+        var iconImg = `https://openweathermap.org/img/wn/${iconCode}.png`
+
+        var htmlTemplate = `<h4 id="current-forecast">Current forecast in <span id="search-city">${data.name} </span><span id="current-date">(${currentDate}):</h4>
+        <ul class="list-unstyled">
+            <li>Temperature: ${data.main.temp}&degF<img src="${iconImg}" /></li>
+            <li>Humidity: ${data.main.humidity}%</li>
+            <li>Wind Speed: ${data.wind.speed}miles per hour</li>
+        </ul>`
+
+        $('#current-weather').append(htmlTemplate);
+        $('#current-weather').addClass('border border-primary rounded p-2');
+
+    })
+    
+}
+
+var updateFiveDayForecast = async (lat, lon) => {
+
+    getFiveDayForecast(lat, lon).then(data => {
+        console.log(data);
+
+        var fiveDay = document.querySelector('#five-day');
+        var sectionHeader = document.createElement('h4');
+        sectionHeader.innerHTML = `5 day forecast for ${data.city.name}:`
+        sectionHeader.className = "w-100 pb-2"
+        fiveDay.appendChild(sectionHeader);
+
+        for (i = 0 ; i < data.list.length ; i+=8) {
+
+            var dt_txt = data.list[i].dt_txt;
+            var date = dt_txt.substr(0, dt_txt.length - 9);
+            
+            var iconCode = data.list[i].weather[0].icon;
+            var iconImg = `https://openweathermap.org/img/wn/${iconCode}.png`
+            
+            var temperature = data.list[i].main.temp;
+            var windSpeed = data.list[i].wind.speed;
+            var humidity = data.list[i].main.humidity;
+
+            // create a container for the cards
+            var card = document.createElement('div');
+            card.className = "card border border-primary mx-1 p-2";
+            
+            // append the card to the five-day section
+            // var fiveDay = document.querySelector('#five-day');
+            fiveDay.appendChild(card);
+            
+            // create ul
+            var ul = document.createElement('ul');
+            ul.className = "list-unstyled text-center";
+            
+            // create date for each card
+            var dateHeader = document.createElement('h5');
+            dateHeader.innerHTML = date;
+            
+            // create listed item for temperature
+            var tempLi = document.createElement('li');
+            tempLi.innerHTML = `Temperature: ${temperature}&degF`
+            
+            // create listed item for img element with source being the iconimg
+            var img = document.createElement('img');
+            img.src = iconImg;
+
+            // create listed item for humidity
+            var humidityLi = document.createElement('li');
+            humidityLi.innerHTML = `Humidity: ${humidity}%`;
+            
+            // create listed item for windSpeed
+            var windSpeedLi = document.createElement('li');
+            windSpeedLi.innerHTML = `Wind Speed: ${windSpeed} mph`;
+
+            card.appendChild(ul)
+            ul.appendChild(dateHeader);
+            ul.appendChild(img);
+            ul.appendChild(tempLi);
+            ul.appendChild(humidityLi);
+            ul.appendChild(windSpeedLi);
+            
+        }       
+
+    })
+}
+
+var previousList = document.querySelector('#previous');
+var buttonList = document.querySelector('.button-list');
+// variable for searched city input
+var cityForm = document.querySelector('form');
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    // get city value
+    var city = cityForm.city.value.trim();
+    
+    // see if city value is what we need
+    console.log(city);
+
+    // reset the search city form
+    cityForm.reset();
+
+    // update with current weather
+    updateCurrentWeather(city);
+    
+    // save to local storage 
+    localStorage.setItem(city, city);
+    var keys = Object.keys(localStorage);
+    // add a button of the last search input
+    addButton(city);
+    
+    
+});
+
+// adds a button after a searched city is saved to localstorage
+function addButton(city) {
+    var button = document.createElement('button');
+    button.innerText = localStorage.getItem(city);
+    button.className = 'block m-2 p-2 cityBtn';
+    buttonList.appendChild(button);
+    // listens for a button click, sends function to pull info back
+    button.addEventListener('click', () => {
+        updateCurrentWeather(city);
+    })    
+};
+
+var renderCities = () => {
+    for (var i = 0; i < localStorage.length; i++) {
+        addButton(localStorage.key(i));       
+    }
+}
+
+renderCities();
+
+function clearAll() {
+    $('#current-weather').empty();
+    $('#five-day').empty();
+};
